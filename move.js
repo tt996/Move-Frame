164ef04da35c3fe07e1004eae2b11ec185f92991
tt996 <2441030477@qq.com>
2020-08-05 00:18:18 +0800

    move1.3版:兼容链式运动

--- move.js
+++ move.js
@@ -1,11 +1,11 @@
 
-// MOVE运动框架1.2
+// MOVE运动框架1.3
 // 以像素为结果的缓冲运动
 // 缓冲运动,兼容px单位
 			// 如margin:10px;属性名(name):属性值(value)
 			// getComputedStyle()只读
 			// target没有单位'px'而name有---采用取整parseInt()解决
-			function move(obj,name,target){
+			function move(obj,name,target,fn){
 				clearInterval(obj.timer)
 				obj.timer=setInterval(function (){
 					// 定义获取值为nowStyle
@@ -27,6 +27,8 @@
 					
 					if (nowStyle==target){
 						clearInterval(obj.timer)
+						// 如果存在第四个变量-true执行
+						if(fn){fn()}
 					}else{
 						obj.style[name]=nowStyle+speed+'px'
 					}
